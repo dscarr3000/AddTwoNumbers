15edb744175a3836570d4bbf66ec81a177e8f5a1
Daniel Carr <54419206+dscarr3000@users.noreply.github.com>
2022-05-09 15:03:29 -0600

    Created solution

--- AddTwoNumbers/main.swift
+++ AddTwoNumbers/main.swift
@@ -1,11 +1,54 @@
-//
-//  main.swift
-//  AddTwoNumbers
-//
-//  Created by Daniel Carr on 5/9/22.
-//
-
 import Foundation
 
-print("Hello, World!")
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() {
+        self.val = 0;
+        self.next = nil;
+    }
+    public init(_ val: Int) {
+        self.val = val;
+        self.next = nil;
+    }
+    public init(_ val: Int, _ next: ListNode?) {
+        self.val = val;
+        self.next = next;
+    }
+ }
 
+func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+    let head = ListNode(0)
+    var p = l1
+    var q = l2
+    var curr = head
+    var carry = 0
+    
+    while p != nil || q != nil {
+        let x = p?.val ?? 0
+        let y = q?.val ?? 0
+        let sum = carry + x + y
+        carry = sum / 10
+        curr.next = ListNode(sum % 10)
+        curr = curr.next!
+        if p != nil {
+            p = p!.next
+        }
+        if q != nil {
+            q = q!.next
+        }
+    }
+    if carry > 0 {
+        curr.next = ListNode(carry)
+    }
+    return head.next
+}
+
+let list1 = ListNode(2, ListNode(4, ListNode(9)))
+let list2 = ListNode(5, ListNode(6, ListNode(4, ListNode(9))))
+let returnList = addTwoNumbers(list1, list2)
+var currentList = returnList
+while currentList != nil {
+    print(currentList!.val)
+    currentList = currentList!.next
+}
